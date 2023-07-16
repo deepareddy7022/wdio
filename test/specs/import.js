c897375ee517bd9fdfa05e4f6fb1107bf0d40752
deepareddy7022 <58291904+deepareddy7022@users.noreply.github.com>
2023-07-16 21:09:26 +0530

    Update import.js

--- test/specs/import.js
+++ test/specs/import.js
@@ -6,7 +6,7 @@ describe("test", () => {
     it("tesrtt", () => {
 
         console.log(url)
-        console.log(process.env.QA)
+        console.log("read data from secrets github actions", process.env.QA)
         printMessage("Hi")
         add(10, 20)
         subtract(30, 10)
